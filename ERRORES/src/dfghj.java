80566fea8545594c772e8e05570efa43058101c4
Benguan85 <benguan18@hotmail.com>
2025-01-29 13:01:00 +0100

    Comentarios
    Signed-off-by: Benguan85 <benguan18@hotmail.com>

---
+++ ERRORES/src/dfghj.java
@@ -0,0 +1,41 @@
+class EjemploEncapsulamiento {
+private int codigo;
+private String nombre;
+private int edad;
+
+public int getCodigo;
+	return codigo
+
+public String getNombre;
+	return nombre
+
+public int getEdad
+	return edad
+
+public void setCodigo int codigo
+	this.codigo=codigo
+
+public void setNombre String nombre
+	this.nombre=nombre
+
+public void setEdad int edad
+	this.edad=edad
+
+
+class PruebaEncapsulamiento
+
+	public static void main String [] args
+
+	EjemploEncapsulamiento obj=new EjemploEncapsulamiento
+
+	obj.setCodigo 12345
+	obj.setNombre “Mario”
+	obj.setEdad 32
+	System.out.println obj.getCodigo
+	System.out.println obj.getNombre
+	System.out.println obj.getEdad
+	
+	//jhdfffffffffffffffffffffffffffffffyt
+	//gjhfffffffffffffffffffff
+	//hgcccccccccccccccc
+	//fjggggggggggggggggggg
